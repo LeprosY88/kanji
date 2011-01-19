1b620b107a8e05415b066b15dcf358bc6c15e619
andrei.bratuhin <andrei.bratuhin@19e6df66-744b-0410-b05e-01f084c5ec6e>
2011-01-19 22:14:43 +0000

    added code for vizualization of stations

--- bahnde/trunk/Bahn.de/src/andrei/bratuhin/frontend/VisualizationPanel2.java
+++ bahnde/trunk/Bahn.de/src/andrei/bratuhin/frontend/VisualizationPanel2.java
@@ -7,11 +7,16 @@ import inf.v3d.view.ViewerPanel;
 import java.util.Iterator;
 import java.util.List;
 
+import org.abratuhi.bahnde.model.Station;
+
+import andrei.bratuhin.model.StationProviderStub;
+
 
 
 
 public class VisualizationPanel2 extends ViewerPanel {
 	private final Frontend frontend;
+	
 
 	public VisualizationPanel2(Frontend frontend) {
 		super();
@@ -19,7 +24,23 @@ public class VisualizationPanel2 extends ViewerPanel {
 		this.frontend = frontend;
 
 		setVisible(true);
+		
 	}
 
 	
+
+public void myrepaint(){
+	clear();
+	List<Station> station = StationProviderStub.getStations();
+	for (Station i : station) {
+		List<Double> coords = i.getCoordinates();
+		Sphere sph = new Sphere( coords.get(0), coords.get(1),  0);
+		sph.setRadius(0.1);
+		sph.setColor(255, 0, 0);
+		sph.register();	
+		
+		addObject3D(sph);
+	}
+}
+
 }
