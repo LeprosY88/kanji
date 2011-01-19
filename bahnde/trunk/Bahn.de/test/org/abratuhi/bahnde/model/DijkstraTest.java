cabba72130fdea5922ea337f5dcca742eda0b834
coiouhkc <coiouhkc@19e6df66-744b-0410-b05e-01f084c5ec6e>
2011-01-19 22:49:17 +0000

    Added: more dijkstra unit tests.

--- bahnde/trunk/Bahn.de/test/org/abratuhi/bahnde/model/DijkstraTest.java
+++ bahnde/trunk/Bahn.de/test/org/abratuhi/bahnde/model/DijkstraTest.java
@@ -9,12 +9,47 @@ import andrei.bratuhin.model.EdgeRouteProviderStub;
 import andrei.bratuhin.model.StationProviderStub;
 
 public class DijkstraTest extends TestCase {
-	public void testGetShortestPath(){
+	public void testGetShortestPath_1_2(){
 		List<Station> stations = StationProviderStub.getStations();
 		List<EdgeRoute> edges = EdgeRouteProviderStub.getEdges();
 		Station from = stations.get(0);
 		Station to = stations.get(1);
 		
 		List<Station> result = Dijkstra.getShortestPath(from, to, stations, edges);
+		
+		assertEquals(2, result.size());
+		assertEquals(from, result.get(0));
+		assertEquals(to, result.get(1));
+	}
+	
+	public void testGetShortestPath_4_1(){
+		List<Station> stations = StationProviderStub.getStations();
+		List<EdgeRoute> edges = EdgeRouteProviderStub.getEdges();
+		Station from = stations.get(4-1);
+		Station to = stations.get(1-1);
+		
+		List<Station> result = Dijkstra.getShortestPath(from, to, stations, edges);
+		
+		assertEquals(4, result.size());
+		assertEquals(from, result.get(0));
+		assertEquals(stations.get(3-1), result.get(1));
+		assertEquals(stations.get(2-1), result.get(2));
+		assertEquals(to, result.get(3));
+	}
+	
+	public void testGetShortestPath_6_14(){
+		List<Station> stations = StationProviderStub.getStations();
+		List<EdgeRoute> edges = EdgeRouteProviderStub.getEdges();
+		Station from = stations.get(6-1);
+		Station to = stations.get(14-1);
+		
+		List<Station> result = Dijkstra.getShortestPath(from, to, stations, edges);
+		
+		assertEquals(5, result.size());
+		assertEquals(from, result.get(0));
+		assertEquals(stations.get(5-1), result.get(1));
+		assertEquals(stations.get(2-1), result.get(2));
+		assertEquals(stations.get(8-1), result.get(3));
+		assertEquals(to, result.get(4));
 	}
 }
