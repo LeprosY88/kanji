a9798e7bf35913baa3e483c5d58aec5b1f55e288
coiouhkc <coiouhkc@19e6df66-744b-0410-b05e-01f084c5ec6e>
2011-01-20 21:43:39 +0000

    Added: box layout

--- bahnde/trunk/Bahn.de/src/andrei/bratuhin/frontend/ReiseDaten.java
+++ bahnde/trunk/Bahn.de/src/andrei/bratuhin/frontend/ReiseDaten.java
@@ -1,19 +1,14 @@
 package andrei.bratuhin.frontend;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
-import javax.swing.JCheckBox;
+import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
-
-import org.abratuhi.bahnde.model.Station;
-
-import andrei.bratuhin.model.StationProviderStub;
 
 public class ReiseDaten extends JPanel {
 	private final Frontend frontend;
@@ -27,27 +22,40 @@ public class ReiseDaten extends JPanel {
 		super();
 		setVisible(true);
 		setBorder(new TitledBorder("Reisedaten"));
-		setSize(new Dimension(200, 200));
-		setLayout(new GridLayout(3, 1));
+		setSize(new Dimension(200, 50));
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
 		String[] items = {"Bochum","Essen","Muelheim/Ruhr","Duisburg", "Oberhausen",
 				"Bottrop", "Gelsenkirchen",	"Hattingen", "Witten", "Dortmund", "Unna",
 				"Schwerte", "Hagen", "Herne"};
 			
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new BoxLayout(panel1, BoxLayout.LINE_AXIS));
+		
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS));
+		
+		JPanel panel3 = new JPanel();
+		panel3.setLayout(new BoxLayout(panel3, BoxLayout.LINE_AXIS));
+		
 		
 
 		Start = new JComboBox(items);
 		Ziel = new JTextField();
 		Abfahrtszeit = new JTextField();
 		
-		add(new JLabel("Start"));
-		add(Start);
-		add(new JLabel("Ziel"));
-		add(Ziel);
-		add(new JLabel("Abfahrtszeit"));
-		add(Abfahrtszeit);
+		panel1.add(new JLabel("Start"));
+		panel1.add(Start);
+		panel2.add(new JLabel("Ziel"));
+		panel2.add(Ziel);
+		panel3.add(new JLabel("Abfahrtszeit"));
+		panel3.add(Abfahrtszeit);
 		this.frontend = frontend;
 
+		add(panel1);
+		add(panel2);
+		add(panel3);
+		
 		repaint();
 	}
 
